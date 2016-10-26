4d639387dcfd14a42b57205450b7c693ba8ed6b4
arun <arunkumar.soundrapandian@gmail.com>
2016-10-26 16:43:27 +0100

    FileFinder implemented

--- src/main/java/uk/gov/justice/tools/FileFinder.java
+++ src/main/java/uk/gov/justice/tools/FileFinder.java
@@ -2,22 +2,48 @@ package uk.gov.justice.tools;
 
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class FileFinder {
 
+    public static final String POM_XML = "pom.xml";
+    public static final String RAML_EXTENSION = ".raml";
     private Config config;
 
     public FileFinder(Config config) {
         this.config = config;
     }
 
-    public List<File> findPomFiles() {
-        return new ArrayList<>();
+    public List<File> findPomFiles() throws IOException {
+        return findFilesMatchingFilename(getConfig().getRootDirectory(), POM_XML);
     }
 
-    public List<File> findRamlFiles() {
-        return new ArrayList<>();
+    public List<File> findRamlFiles() throws IOException {
+        return findFilesMatchingFilename(getConfig().getRootDirectory(), RAML_EXTENSION);
+    }
+
+    public Config getConfig() {
+        return config;
+    }
+
+    public void setConfig(Config config) {
+        this.config = config;
+    }
+
+    private List<File> findFilesMatchingFilename(String path, String filenamePattern) throws IOException {
+        List<File> files = new ArrayList<>();
+        Files.find(Paths.get(path), Integer.MAX_VALUE,
+                (p, bfa) -> bfa.isRegularFile() && p.endsWith(filenamePattern))
+                .map(p -> p.toFile())
+                .forEach(files::add);
+        return files;
     }
 }
--- src/test/java/uk/gov/justice/tools/FileFinderTest.java
+++ src/test/java/uk/gov/justice/tools/FileFinderTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import org.junit.Before;
@@ -12,27 +13,27 @@ import org.junit.Test;
 
 public class FileFinderTest {
 
+    private static final String ROOT_FOLDER = "root";
+
     FileFinder testObj;
 
     @Before
     public void setup() {
+        ClassLoader classLoader = getClass().getClassLoader();
         Config config = new Config();
+        config.setRootDirectory(classLoader.getResource("./".concat(ROOT_FOLDER)).getPath());
         testObj = new FileFinder(config);
     }
 
     @Test
-    public void testFindPomFiles() {
-
+    public void testFindPomFiles() throws IOException {
         List<File> pomFiles = testObj.findPomFiles();
-
-        assertThat(pomFiles.size(), is(0));
+        assertThat(pomFiles.size(), is(2));
     }
 
     @Test
-    public void testFindRamlFiles() {
-
+    public void testFindRamlFiles() throws IOException {
         List<File> ramlFiles = testObj.findRamlFiles();
-
         assertThat(ramlFiles.size(), is(0));
     }
 }

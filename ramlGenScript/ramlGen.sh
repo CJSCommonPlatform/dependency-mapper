7765b5003707b43e6d08b85a58397fafb0b3a8d8
David Simons <david.simons@hmcts.net>
2016-10-28 16:17:28 +0100

    RamlGen script should have parametrised source and target dirs

--- ramlGenScript/ramlGen.sh
+++ ramlGenScript/ramlGen.sh
@@ -1,33 +1,31 @@
 #!/bin/sh
 
-function MungeFile() {
-
-  dirName=$(dirname $1)
-  fileName=$(basename $1)
-  newFileName=${fileName/".raml"/".html"}
-
-  if grep -q "message:\/\/" $1 ; then
-    mkdir "ramlDocs/$fileName.temp/"
-    cp -a "$dirName/." "ramlDocs/$fileName.temp/"
-    sed -i.bak s/"message:\/\/"/"http:\/\/"/g "ramlDocs/$fileName.temp/$fileName"
-    GenerateFile "ramlDocs/$fileName.temp/$fileName" "ramlDocs/$newFileName"
-    sed -i.bak s/"<\/h1>"/"<\/h1><div class=\"alert alert-warning\">Due to technical limitations, this is listed as a HTTP endpoint\. Please note that this is as an asynchronous endpoint that uses the 'message:\/\/' protocol.<\/div>"/g "ramlDocs/$newFileName"
-    rm -rf "ramlDocs/$fileName.temp/"
-  else
-    GenerateFile $1 "ramlDocs/$newFileName"
-  fi
-
-}
+if [ "$#" -ne 2 ]; then
+  echo "Unexpected number of arguments"
+  echo "     usage: ramlGen [sourcePath] [targetPath]"
+  exit 1;
+fi
 
 function GenerateFile() {
-  raml2html $1 > $2 2> "$2.error"
+  dirName=$(dirname $2)
+  fileName=$(basename $2)
+  newFileName=${fileName/".raml"/".html"}
+
+  if grep -q "message:\/\/" $2 ; then
+    #if the raml uses the message protocol it will be parsed as invalid by raml2html. we should replace this and add a warning
+    mkdir "$1/$fileName.temp/"
+    cp -a "$dirName/." "$1/$fileName.temp/"
+    sed -i.bak s/"message:\/\/"/"http:\/\/"/g "$1/$fileName.temp/$fileName"
+    raml2html "$1/$fileName.temp/$fileName" > "$1/$newFileName" 2> "$1/$newFileName.error"
+    sed -i.bak s/"<\/h1>"/"<\/h1><div class=\"alert alert-warning\">Due to technical limitations, this is listed as a HTTP endpoint\. Please note that this is as an asynchronous endpoint that uses the 'message:\/\/' protocol.<\/div>"/g "$1/$newFileName"
+    rm -rf "$1/$fileName.temp/"
+  else
+    raml2html $2 > "$1/$newFileName" 2> "$1/$newFileName.error"
+  fi
 }
 
-export -f MungeFile
 export -f GenerateFile
-
-mkdir ramlDocs
-
-find . -name *.raml | xargs -L1 bash -c 'MungeFile "$@"' _
-find ramlDocs/ -size  0 -print0 |xargs -0 rm
-rm ramlDocs/*.bak
+mkdir -p "$2"
+find $1 -name 'cpp.context.*' | xargs -I {} find {} -name '*.raml' | xargs -L1 bash -c 'GenerateFile "$@"' _ $2
+find $2 -size  0 -print0 | xargs -0 rm
+rm $2/*.bak

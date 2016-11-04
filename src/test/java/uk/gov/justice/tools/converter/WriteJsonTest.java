18e57a76e4a6d9cc3a341058196a67d90ab393e4
Hasan.shaik <hasan.shaik@ext.soprasteria.com>
2016-11-04 15:23:08 +0000

    Refactoring : removing temp dir and changing root folder

--- src/test/java/uk/gov/justice/tools/converter/WriteJsonTest.java
+++ src/test/java/uk/gov/justice/tools/converter/WriteJsonTest.java
@@ -1,33 +1,54 @@
 package uk.gov.justice.tools.converter;
 
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-import uk.gov.justice.tools.Config;
-
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 import org.junit.Test;
 
+import uk.gov.justice.tools.Config;
+
 public class WriteJsonTest {
 
-    @Test
-    public void convert() throws Exception {
+	@Test
+	public void convert() throws Exception {
 
-        Path tempDir = Files.createTempDirectory("dmx");
+		Path tempDir = Files.createTempDirectory("dmx");
 
-        Config config = new Config();
-        config.setOutputFilePath(tempDir.toString() + "/contexts.json");
+		Config config = new Config();
+		config.setOutputFilePath(tempDir.toString() + "/contexts.json");
 
-        WriteJson testObj = new WriteJson(config);
+		WriteJson testObj = new WriteJson(config);
 
-        Path path = testObj.convert("{\"name\":\"abc\",\"version\":\"1.1\"}");
+		Path path = testObj.convert("{\"name\":\"abc\",\"version\":\"1.1\"}");
 
-        assertThat(new String(Files.readAllBytes(path)), is("{\"name\":\"abc\",\"version\":\"1.1\"}"));
+		assertThat(new String(Files.readAllBytes(path)), is("{\"name\":\"abc\",\"version\":\"1.1\"}"));
 
-        //delete temp dir?
-    }
+		recursiveDeleteOnExit(tempDir);
+
+	}
+
+	public static void recursiveDeleteOnExit(Path rootPath) throws IOException {
+		Files.walkFileTree(rootPath, new SimpleFileVisitor<Path>() {
+			@Override
+			public FileVisitResult visitFile(Path file, @SuppressWarnings("unused") BasicFileAttributes attrs) {
+				file.toFile().delete();
+				return FileVisitResult.CONTINUE;
+			}
+
+			@Override
+			public FileVisitResult preVisitDirectory(Path dir, @SuppressWarnings("unused") BasicFileAttributes attrs) {
+				dir.toFile().delete();
+				return FileVisitResult.CONTINUE;
+			}
+		});
+		Files.delete(rootPath);
+	}
 
 }
